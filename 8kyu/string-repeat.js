a0fe1bb6be1a062f0ed9732fd7a718b88867b7ae
Sarah <xsarahyu@gmail.com>
2023-10-06 22:53:51 -0400

    added prompt to string-repeat.js

--- 8kyu/string-repeat.js
+++ 8kyu/string-repeat.js
@@ -1,3 +1,11 @@
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
 function repeatStr (num, str) {
     let repeatStr = ""
     for (i=1; i<=num; i++) {
