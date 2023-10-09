69eafc5fa6654585db19041b9d1e7f4c228e43ec
Sarah <xsarahyu@gmail.com>
2023-10-08 22:03:05 -0400

    added copy-paperwork.js & sum-mixed-array.js

---
+++ 8kyu/sum-mixed-array.js
@@ -0,0 +1,20 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(arr) {
+    arr = arr.map(Number)
+    let sum = 0
+    for (i=0; i<arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum
+}
+
+//or
+
+let sumMix = arr => arr.reduce((a, b) => a + +b, 0)
+//arr.map(Number) not needed, as the unary plus operator +b converts elements to numbers
+//0 is initial value, so code doesn't break if first item of array is string
