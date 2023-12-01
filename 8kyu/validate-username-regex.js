beb9d064997a41f481fa9e08635f28176007590d
Sarah <xsarahyu@gmail.com>
2023-11-30 19:03:04 -0500

    add validate-username-regex.js

---
+++ 8kyu/validate-username-regex.js
@@ -0,0 +1,10 @@
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+
+const validateUsr = username => /^([a-z_0-9]){4,16}$/.test(username)
